3d45bc8209195550481a8d8df39647b6f3e07008
Snad_ <darkusolomon1@gmail.com>
2018-08-20 10:15:44 +0000

    My

--- resources/views/parent/sidebar.blade.php
+++ resources/views/parent/sidebar.blade.php
@@ -1,5 +1,108 @@
 <li  class="active" >
-    <a href="dashboard.html">
+    <a href={{route('home')}}>
+        <i class="now-ui-icons design_app"></i>
+        <p>Dashboard</p>
+    </a>
+</li>
+
+<li>
+    <a data-toggle="collapse" href="#academics" >
+        <i class="now-ui-icons education_hat"></i>
+        <p>
+            Academics <b class="caret"></b>
+        </p>
+    </a>
+    <div class="collapse " id="academics">
+        <ul class="nav">
+            <li>
+                <a href="#">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</li>
+
+<li>
+    <a data-toggle="collapse" href="#attendance" >
+        <i class="now-ui-icons location_world"></i>
+        <p>
+            Attendance <b class="caret"></b>
+        </p>
+    </a>
+    <div class="collapse " id="attendance">
+        <ul class="nav">
+            <li>
+                <a href="#">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</li>
+
+<li>
+    <a data-toggle="collapse" href="#student" >
+        <i class="now-ui-icons users_single-02"></i>
+        <p>
+            Students <b class="caret"></b>
+        </p>
+    </a>
+    <div class="collapse " id="student">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</li>
+
+<li>
+    <a data-toggle="collapse" href="#finance" >
+        <i class="now-ui-icons business_bank"></i>
+        <p>
+            Finance <b class="caret"></b>
+        </p>
+    </a>
+    <div class="collapse " id="finance">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</li>
+
+
+<li>
+    <a data-toggle="collapse" href="#event" >
+        <i class="now-ui-icons location_world"></i>
+        <p>
+            Events <b class="caret"></b>
+        </p>
+    </a>
+    <div class="collapse " id="event">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</li>
+
+<li >
+    <a href="#">
         <i class="now-ui-icons design_app"></i>
         <p>Dashboard</p>
     </a>
