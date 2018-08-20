193c5d38c9beea7f7d7c3c5dfc877b8a52a63ad4
perez cato <perezcatoc@gmail.com>
2018-08-20 11:07:36 +0100

    Finished the admin sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,114 +5,161 @@
     </a>
 </li>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="now-ui-icons location_world"></i>
+<li>
+    <a data-toggle="collapse" href="#pagesExamples" >
+        <i class="now-ui-icons education_hat"></i>
         <p>
-            <span class="d-lg-none d-md-block">Academics</span>
+            Academics <b class="caret"></b>
         </p>
     </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="collapse " id="pagesExamples">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
     </div>
 </li>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="now-ui-icons location_world"></i>
+
+
+<li>
+    <a data-toggle="collapse" href="#student" >
+        <i class="now-ui-icons users_single-02"></i>
         <p>
-            <span class="d-lg-none d-md-block">Student</span>
+            Students <b class="caret"></b>
         </p>
     </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="collapse " id="student">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
     </div>
 </li>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="now-ui-icons location_world"></i>
+<li>
+    <a data-toggle="collapse" href="#finance" >
+        <i class="now-ui-icons business_bank"></i>
         <p>
-            <span class="d-lg-none d-md-block">Finance</span>
+            Finance <b class="caret"></b>
         </p>
     </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="collapse " id="finance">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
     </div>
 </li>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<li>
+    <a data-toggle="collapse" href="#messages" >
         <i class="now-ui-icons location_world"></i>
         <p>
-            <span class="d-lg-none d-md-block">Messages/SMS</span>
+            Messages/SMS <b class="caret"></b>
         </p>
     </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="collapse " id="messages">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
     </div>
 </li>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="now-ui-icons location_world"></i>
+<li>
+    <a data-toggle="collapse" href="#performance" >
+        <i class="now-ui-icons business_chart-bar-32"></i>
         <p>
-            <span class="d-lg-none d-md-block">Performance</span>
+            Performance <b class="caret"></b>
         </p>
     </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="collapse " id="performance">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
     </div>
 </li>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="now-ui-icons location_world"></i>
+<li>
+    <a data-toggle="collapse" href="#events" >
+        <i class="now-ui-icons ui-2_like"></i>
         <p>
-            <span class="d-lg-none d-md-block">Events</span>
+            Events <b class="caret"></b>
         </p>
     </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="collapse " id="events">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
     </div>
 </li>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="now-ui-icons location_world"></i>
+<li>
+    <a data-toggle="collapse" href="#reports" >
+        <i class="now-ui-icons education_paper"></i>
         <p>
-            <span class="d-lg-none d-md-block">Reports</span>
+            Reports <b class="caret"></b>
         </p>
     </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="collapse " id="reports">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
     </div>
 </li>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="now-ui-icons location_world"></i>
+<li>
+    <a data-toggle="collapse" href="#parents" >
+        <i class="now-ui-icons users_circle-08"></i>
         <p>
-            <span class="d-lg-none d-md-block">Guardians/ Parents</span>
+            Guardians <b class="caret"></b>
         </p>
     </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="collapse " id="parents">
+        <ul class="nav">
+            <li>
+                <a href="pages/pricing.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Pricing </span>
+                </a>
+            </li>
+        </ul>
     </div>
 </li>
+
+
+
+
+
